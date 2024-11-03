e0de1b7f65639523a6817dd6cbab90fc7eafbb0a
Leo <volwahsenl@gmail.com>
2024-11-03 12:37:25 +0100

    updated api calls and added try-catch to handle errors and preventing server from crashing

--- backend/index.js
+++ backend/index.js
@@ -36,126 +36,190 @@ async function run() {
 
     // classes routes here
     app.post("/new-class", async (req, res) => {
+      try {
         const newClass = req.body;
         // newClass.availableSeats = parseInt(newClass.availableSeats);
         const result = await classesCollection.insertOne(newClass);
         res.send(result);
-    });
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "An error occurred" });
+      }
+    })
 
     app.get("/classes", async (req, res) => {
+      try {
         const query = { status: "approved" };
         const result = await classesCollection.find().toArray()
         res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "An error occurred" });
+      }
     })
 
     // Get classes by instructor email address
     app.get("/classes/:email", async (req, res) => {
+      try {
         const email = req.params.email;
         const query = { instructorEmail: email };
         const result = await classesCollection.find(query).toArray();
         res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "An error occurred" });
+      }
     })
 
     // manage classes
-    app.get(`/classes-manage`, async(req, res) => {
-      const result = await classesCollection.find().toArray();
-      res.send(result);
+    app.get(`/classes-manage`, async (req, res) => {
+      try {
+        const result = await classesCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "An error occurred" });
+      }
     })
 
     // update classes status reason
     app.patch('/change-status/:id', async (req, res) => {
-      const id = req.params.id;
-      const status = req.body.status;
-      const reason = req.body.reason;
-      const filter = { _id: new ObjectId(id) };
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: {
-          status: status,
-          reason: reason,
-        },
-      };
-      const result = await classesCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      try {
+        const id = req.params.id;
+        const status = req.body.status;
+        const reason = req.body.reason;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            status: status,
+            reason: reason,
+          },
+        };
+        const result = await classesCollection.updateOne(filter, updateDoc, options);
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "An error occurred" });
+      }
     })
 
     // get approved classes
-    app.get('/approved-classes', async(req,res) => {
-      const query = { status: "approved" };
-      const result = await classesCollection.find(query).toArray();
-      res.send(result);
+    app.get('/approved-classes', async (req, res) => {
+      try {
+        const query = { status: "approved" };
+        const result = await classesCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "An error occurred" });
+      }
     })
 
     // get single class details
     app.get('/class/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await classesCollection.findOne(query);
-      res.send(result);
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await classesCollection.findOne(query);
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "An error occurred" });
+      }
     })
 
     // update class details and override entire body of object
     app.put('/update-class/:id', async (req, res) => {
-      const id = req.params.id;
-      const updateClass = req.body;
-      const filter = { _id: new ObjectId(id) };
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: {
-          name: updateClass.name,
-          description: updateClass.description,
-          price: updateClass.price,
-          availableSeats: parseInt(updateClass.availableSeats),
-          videoLink: updateClass.videoLink,
-          status: 'pending'
-        }
-      };
-      const result = await classesCollection.updateOne(filter, updateDoc, options);
-      res.send(result)
+      try {
+        const id = req.params.id;
+        const updateClass = req.body;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            name: updateClass.name,
+            description: updateClass.description,
+            price: updateClass.price,
+            availableSeats: parseInt(updateClass.availableSeats),
+            videoLink: updateClass.videoLink,
+            status: 'pending'
+          }
+        };
+        const result = await classesCollection.updateOne(filter, updateDoc, options);
+        res.send(result)
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "An error occurred" });
+      }
     })
 
     // Cart Routes
     app.post('/add-to-cart', async (req, res) => {
-      const newCartItem = req.body;
-      const result = await cartCollection.insertOne(newCartItem);
-      res.send(result);
+      try {
+        const newCartItem = req.body;
+        const result = await cartCollection.insertOne(newCartItem);
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "An error occurred" });
+      }
     })
 
     // get cart item by id
     app.get('/cart-item/:id', async (req, res) => {
-      const id = req.params.id;
-      const email = req.body.instructorEmail;
-      const query = {
-        classId: id,
-        userMail: email
-      };
-      const projection = { classId: 1 };
-      const result = await cartCollection.findOne(query, {projection: projection});
-      res.send(result);
+      try {
+        const id = req.params.id;
+        const email = req.body.email;
+        const query = {
+          classId: id,
+          userMail: email
+        };
+        const projection = { classId: 1 };
+        const result = await cartCollection.findOne(query, { projection: projection });
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "An error occurred" });
+      }
     })
 
     // cart info by user email
-    app.get('/cart/email', async (req, res) => {
-      const email= req.params.email;
-      const query = { userMail: email };
-      const projection = { classId: 1 };
-      const carts = cartCollection.find(query, { projection: projection });
-      const classIds = carts.map((cart) => new ObjectId(cart.classId));
-      const query2 = { _id: { $in: classIds }};
-      const result = await classesCollection.find(query2).toArray();
-      res.send(result);
-    })
+    app.get('/cart/:email', async (req, res) => {
+      try {
+        const email = req.params.email;
+        const query = { userMail: email };
+        const projection = { classId: 1 };
+
+        const carts = await cartCollection.find(query, { projection }).toArray();
+        const classIds = carts.map(cart => new ObjectId(cart.classId));
+
+        const query2 = { _id: { $in: classIds } };
+        const result = await classesCollection.find(query2).toArray();
+
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "An error occurred" });
+      }
+    });
+
 
     // delete cart item
     app.delete('/delete-cart-item/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { classId: id };
-      const result = await cartCollection.deleteOne(query);
-      res.send(result);
+      try {
+        const id = req.params.id;
+        const query = { classId: id };
+        const result = await cartCollection.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "An error occurred" });
+      }
     })
 
     // Payments route
-    
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
